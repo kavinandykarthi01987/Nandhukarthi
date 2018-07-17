fa1f4e7b41717681872578b8f6659165629cbd89
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-17 15:46:09 +0530

    Create multiple.java

---
+++ multiple.java
@@ -0,0 +1,9 @@
+public class multiple {
+	public static void main(String[] args) {
+   Scanner s=new Scanner(System.in);
+   int n=s.nextInt();
+   for(int i=1;i<=5;i++){
+	   System.out.print(i*n +" ");
+   }
+	}
+}
