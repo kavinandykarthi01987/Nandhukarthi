05280eaaf27b818d7e14e0ba640ca12c20e3bfb7
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-17 15:53:39 +0530

    Create maximumnumber.java

---
+++ maximumnumber.java
@@ -0,0 +1,12 @@
+public class maxarrayelement {
+	public static void main(String[] args) {
+     Scanner s=new Scanner(System.in);
+     int n=s.nextInt();
+     int a[]=new int[n];
+     for(int i=0;i<n;i++){
+    	 a[i]=s.nextInt();
+     }
+     Arrays.sort(a);
+     System.out.println(a[n-1]);
+	}
+}
