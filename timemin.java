93c5f585f0dfb0238629ff348436b50c35b20e84
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-19 15:07:12 +0530

    Update timemin.java

--- timemin.java
+++ timemin.java
@@ -1,13 +1,9 @@
 public class time {
   public static void main(String[] args) {
-		String a="10:50";
-		String b="9:20";
-		String str1[]=a.split(":");
-		String str2[]=b.split(":");
-		int t1=Integer.valueOf(str1[0]);
-		int t2=Integer.valueOf(str2[0]);
-		int m1=Integer.valueOf(str1[1]);
-		int m2=Integer.valueOf(str2[1]);
+		int t1=10;
+		int m2=50;
+		int t2=9;
+		int m1=20;
 		System.out.println(Math.abs(t1-t2)+" "+Math.abs(m1-m2));
 	}
 
