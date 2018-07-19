c4cb16c658c9e2547d45ff5b85c01db2de88e6b3
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-19 15:13:22 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,17 @@
+package distin;
+
+import java.util.Scanner;
+
+public class index {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in); 
+    	int n=s.nextInt();
+         int a[]=new int[n];
+         for(int i=0;i<n;i++){
+        	 a[i]=s.nextInt();
+         }
+         for(int i=0;i<n;i++){
+         System.out.println(a[i] +" " +i);
+           }
+         }
+}
