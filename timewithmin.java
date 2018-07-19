f359965c975d52af36af5a677c791820c08ee8a3
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-19 15:40:37 +0530

    Create timewithmin.java

---
+++ timewithmin.java
@@ -0,0 +1,12 @@
+package distin;
+import java.util.*;
+public class timessss {
+  public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int time=n/60;
+		int min=n%60;
+		System.out.println(time +" " +min);
+	}
+
+}
