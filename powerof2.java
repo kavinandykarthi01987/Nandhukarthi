e08dbb4fdc788f7477ca947685464903308d7f20
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-20 13:54:49 +0530

    Create powerof2.java

---
+++ powerof2.java
@@ -0,0 +1,13 @@
+package stringnumber;
+import java.util.*;
+public class powerof2 {
+	public static void main(String[] args) {
+     Scanner s=new Scanner(System.in);
+     int n=s.nextInt();
+     if(n%2==0){
+    	 System.out.println("YES");
+     }else{
+       System.out.println("NO");	 
+     }
+	}
+}
