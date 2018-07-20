2afb0954e1c34ad4d6a7ee54f7d3f08d30558b61
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-20 15:55:36 +0530

    Create countingnumberdigit.java

---
+++ countingnumberdigit.java
@@ -0,0 +1,9 @@
+package guvi;
+public class countingnumberdigit {
+public static void main(String[] args){
+	long n=1234567890;
+	String s=String.valueOf(n);
+	int l=s.length();
+	System.out.println(l);
+}
+}
