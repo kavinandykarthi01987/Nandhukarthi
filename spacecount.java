3225006e4fbf8f3fb5043c313a6d5809b5cf9602
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-20 09:22:23 +0530

    Create spacecount.java

---
+++ spacecount.java
@@ -0,0 +1,11 @@
+package guvi;
+import java.util.*;
+public class counting {
+public static void main(String[] args) {
+	 Scanner s=new Scanner(System.in);
+	 String input=s.nextLine();
+	 String str[]=input.split(" ");
+	 int count=str.length;
+   System.out.println(count-1);
+	}
+}
