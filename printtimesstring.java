27208c95fb7d51902110dc56402cbc54ebfe0095
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-20 10:28:34 +0530

    Create printtimesstring.java

---
+++ printtimesstring.java
@@ -0,0 +1,12 @@
+package stringnumber;
+import java.util.*;
+public class printstring {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String word=s.next();
+		int n=s.nextInt();
+		for(int i=0;i<n;i++){
+			System.out.println(word);
+		}
+	}
+}
