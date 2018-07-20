307d0986f194a3724f54a152868d63dff1068778
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-20 09:37:14 +0530

    Create numbercountsint.java

---
+++ numbercountsint.java
@@ -0,0 +1,11 @@
+package guvi;
+import java.util.*;
+public class numeric{
+public static void main(String[] args) {
+	 Scanner s=new Scanner(System.in);
+	 int n=s.nextInt();
+	 String str=String.valueOf(n);
+	 int count=str.length();
+   System.out.println(count);
+	}
+}
