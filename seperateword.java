d4f0db611c2265f146cf95cd4f694d85af02cce5
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-20 17:03:20 +0530

    Create seperateword.java

---
+++ seperateword.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class seperate {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String word=s.next();
+		int n=s.nextInt();
+		System.out.println(word.substring(0,n));
+	}
+}
