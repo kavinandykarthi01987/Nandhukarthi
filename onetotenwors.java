7084ad88702173c35303fa7e5be3a7bb6cbef7c1
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-20 15:27:51 +0530

    Create onetotenwors.java

---
+++ onetotenwors.java
@@ -0,0 +1,8 @@
+public class numberinword {
+	public static void main(String[] args) {
+      String ot[]={"zero","one","two","three","four","five","six","seven","eight","nine","ten"};
+      Scanner s=new Scanner(System.in);
+	  int n=s.nextInt();
+	  System.out.println(ot[n]);
+	}
+  }
