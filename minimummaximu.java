e85383942d08273d25189a60f262bb9119480bf8
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-20 13:42:21 +0530

    Create minimummaximu.java

---
+++ minimummaximu.java
@@ -0,0 +1,14 @@
+package stringnumber;
+import java.util.*;
+public class minimummaximum {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=s.nextInt();
+		}
+		Arrays.sort(a);
+		System.out.println(a[0] +" " +a[n-1]);
+	}
+}
