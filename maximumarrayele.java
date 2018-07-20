235292d87a5b742faff3d675e785cbe44600352e
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-20 10:17:44 +0530

    Update maximumarrayele.java

--- maximumarrayele.java
+++ maximumarrayele.java
@@ -3,12 +3,11 @@ import java.util.*;
 public class max {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int n=s.nextInt();
-		int a[]=new int[n];
-		for(int i=0;i<n;i++){
+		int a[]=new int[10];
+		for(int i=0;i<10;i++){
 			a[i]=s.nextInt();
 		}
 		Arrays.sort(a);
-		System.out.println(a[n-1]);
+		System.out.println(a[9]);
 	}
 }
