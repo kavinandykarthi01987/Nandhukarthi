e2e18c265c9d94c81e6e8403c7f70f11a370233b
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-20 15:37:17 +0530

    Create evenoddproduct.java

---
+++ evenoddproduct.java
@@ -0,0 +1,15 @@
+package stringnumber;
+import java.util.*;
+public class evenoddproduct {
+  public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	int num=s.nextInt();
+	int num1=s.nextInt();
+	int num3=num*num1;
+	if(num3%2==0){
+		System.out.println("EVEN");
+	}else{
+		System.out.println("ODD");
+	}
+	}
+}
