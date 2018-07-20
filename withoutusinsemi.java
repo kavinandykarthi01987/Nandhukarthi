0e8fa57f17b38da54bf8b74331abf36f421ca537
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-20 17:45:26 +0530

    Create withoutusinsemi.java

---
+++ withoutusinsemi.java
@@ -0,0 +1,11 @@
+package studentmark;
+import java.util.*;
+public class withoutusingsemicoloan {
+	public static void main(String[] args) {
+    Scanner s=new Scanner(System.in);
+    String word=s.nextLine();
+    for(int i=1;i<=1;System.out.println(word)){
+        i++;
+	}
+	}
+}
