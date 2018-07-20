d5875220e9825e8a48285d52a392c226beaaf027
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-20 13:51:45 +0530

    Create sumoftwo.java

---
+++ sumoftwo.java
@@ -0,0 +1,10 @@
+package stringnumber;
+import java.util.*;
+public class sum {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int s1=s.nextInt();
+		int s2=s.nextInt();
+		System.out.println(s1+s2);
+	}
+}
