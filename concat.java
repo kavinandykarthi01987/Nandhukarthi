4b26748367cc28895afeee757a27fcd5abbadec5
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-20 12:09:25 +0530

    Create concat.java

---
+++ concat.java
@@ -0,0 +1,10 @@
+package stringnumber;
+import java.util.*;
+public class greaterstring {
+	public static void main(String[] args) {
+    Scanner s=new Scanner(System.in);
+    String s1=s.next();
+    String s2=s.next();
+     System.out.println(s1+s2);
+}
+}
