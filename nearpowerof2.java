28170d8c1c391b5c11c9d6978087c46fe9308ee2
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-21 09:41:31 +0530

    Create nearpowerof2.java

---
+++ nearpowerof2.java
@@ -0,0 +1,9 @@
+package stringnumber;
+import java.util.*;
+public class nearpowerof2 {
+	public static void main(String[] args) {
+     Scanner s=new Scanner(System.in);
+     int n=s.nextInt();
+    	 System.out.println(n*2);
+     }
+}
