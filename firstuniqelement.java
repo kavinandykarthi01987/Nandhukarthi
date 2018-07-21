4b8c06872000b8ea6f9740bc354d566a54a1ed47
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-21 14:26:33 +0530

    Create firstuniqelement.java

---
+++ firstuniqelement.java
@@ -0,0 +1,25 @@
+package hunter;
+import java.util.*;
+public class firstuniqelement {
+ public static void main(String[] args){
+	 Scanner s=new Scanner(System.in);
+	 int n=s.nextInt();
+	 int a[]=new int[n];
+	 TreeSet uniq=new TreeSet();
+	 for(int i=0;i<n;i++){
+		 a[i]=s.nextInt();
+	 }
+	 for(int i=0;i<n;i++){
+		 int count=0;
+		 for(int j=0;j<n;j++){
+			 if(a[i]==a[j]){
+				 count++;
+			 }
+		 }
+		 if(count>1){
+			 System.out.println(a[i]);
+			 break;
+		 }
+	 }
+ }
+}
