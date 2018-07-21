6582278af6d91a22430227b313e92456eabb6f9a
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-21 14:30:02 +0530

    Create evennumoddpos.java

---
+++ evennumoddpos.java
@@ -0,0 +1,20 @@
+package hunter;
+import java.util.*;
+public class indexvalue {
+	public static void main(String[] args) {
+         Scanner s=new Scanner(System.in);
+         int n=s.nextInt();
+         int a[]=new int[n];
+         for(int i=0;i<n;i++){
+        	 a[i]=s.nextInt();
+         }
+         for(int i=0;i<n;i++){
+        	 if(i%2!=0 && a[i]%2==0){
+        		 System.out.print(a[i] +" ");
+        	 }
+        	 if(i%2==0 && a[i]%2!=0){
+        		 System.out.print(a[i] +" ");
+        	 }
+         }
+	}
+}
