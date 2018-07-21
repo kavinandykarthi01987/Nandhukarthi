300590ff80239f779ad8a10e90ca5bd2e4cdbd7f
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-21 14:17:34 +0530

    Rename index.java to indexposition.java

---
+++ indexposition.java
@@ -0,0 +1,18 @@
+package hunter;
+import java.util.*;
+public class indexvalue {
+	public static void main(String[] args) {
+         Scanner s=new Scanner(System.in);
+         int n=s.nextInt();
+         int a[]=new int[n];
+         for(int i=0;i<n;i++){
+        	 a[i]=s.nextInt();
+         }
+         for(int i=0;i<n;i++){
+        	 if(i==a[i]){
+        		 System.out.print(a[i] +" ");
+        	 }
+         }
+	}
+
+}
