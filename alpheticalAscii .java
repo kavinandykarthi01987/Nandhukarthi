cf3f388bacddf6121851779c6f99a7f5ef0d20b7
kavinandykarthi01987 <41136654+kavinandykarthi01987@users.noreply.github.com>
2018-07-24 11:19:13 +0530

    Create alpheticalAscii .java

---
+++ alpheticalAscii .java
@@ -0,0 +1,9 @@
+package playerguvi;
+import java.util.*;
+public class alpheticalAscii {
+	public static void main(String[] args) {
+     Scanner s=new Scanner(System.in);
+     char ch=s.next().charAt(0);
+     System.out.println((int)ch);
+	}
+}
